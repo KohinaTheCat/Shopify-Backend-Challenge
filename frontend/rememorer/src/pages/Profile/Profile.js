67d64346d22d90e64746f8829ffc99b7f1c63d11
Clara Chick <clara.chick@mail.utoronto.ca>
2020-12-26 21:35:53 -0500

    image upload works for one user

--- frontend/rememorer/src/pages/Profile/Profile.js
+++ frontend/rememorer/src/pages/Profile/Profile.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "./profile.css";
-import { Upload } from "element-react";
+import { Upload, Message } from "element-react";
 import Nav from "../../components/Nav/Nav";
+import UploadService from "../../services/user";
 
 const Profile = () => {
-  const handleRemove = (file, fileList) => {
-    console.log(file, fileList);
+  const handleAvatarScucess = (res, file) => {
+    console.log(URL.createObjectURL(file.raw));
   };
 
-  const handlePictureCardPreview = (file) => {
-    this.setState({
-      dialogImageUrl: file.url,
-      dialogVisible: true,
+  const beforeAvatarUpload = (file) => {
+    console.log(file);
+    const isJPG = file.type === "image/jpeg";
+    const isLt2M = file.size / 1024 / 1024 < 2;
+
+    if (!isJPG) {
+      Message("the file type is not a JPG");
+    }
+    if (!isLt2M) {
+      Message("the file type is greater tahn 2MB");
+    }
+
+    UploadService.upload(file).then(() => {
+      return isJPG && isLt2M;
     });
   };
 
@@ -22,14 +33,19 @@ const Profile = () => {
         <div class="text-area">
           <p>this is some tesxt</p>
         </div>
-        <img alt="img" src="http://localhost:5000/files/get/5fe7e0c7b90e333f28211da4" />
+        <img
+          class="rand-img"
+          alt="img"
+          src="http://localhost:5000/files/get/5fe7e0c7b90e333f28211da4"
+        />
         <Upload
-          action="//jsonplaceholder.typicode.com/posts/"
-          listType="picture-card"
-          onPreview={(file) => this.handlePictureCardPreview(file)}
-          onRemove={(file, fileList) => this.handleRemove(file, fileList)}
+          className="avatar-uploader"
+          action=""
+          showFileList={false}
+          onSuccess={(res, file) => handleAvatarScucess(res, file)}
+          beforeUpload={(file) => beforeAvatarUpload(file)}
         >
-          <i className="el-icon-plus"></i>
+          <i className="el-icon-plus avatar-uploader-icon"></i>
         </Upload>
       </div>
       {/* <el-button type="danger" round>reminisce some more</el-button> */}
